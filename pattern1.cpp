6451d8bc189d2ecdc5358206f14857ed6368a20e
Anish Nilajkar <anishsnilajkar@gmail.com>
2024-08-08 13:44:11 +0530

    Creating simple pattern 1

--- pattern1.cpp
+++ pattern1.cpp
@@ -13,8 +13,7 @@ pattern 1
 using namespace std;
 int main(){
     int x,i,j;
-    cin>>x;  
-    //x=5
+    cin>>x;  //x=5
     for(i=0;i<x;i++){
         for(j=0;j<x;j++){
             cout<<"* ";
