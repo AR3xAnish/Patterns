9493c3fa0e5adc8a4f790fd69f7b8329c178f8f6
Anish Nilajkar <anishsnilajkar@gmail.com>
2024-08-08 13:40:14 +0530

    Create pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,25 @@
+/*
+
+pattern 1
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * *
+
+*/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x,i,j;
+    cin>>x;  
+    //x=5
+    for(i=0;i<x;i++){
+        for(j=0;j<x;j++){
+            cout<<"* ";
+        };
+        cout<<endl;
+    };
+    return 0;
+}
