491c537fb50cb2f4d65adc1becafa356727b56d0
Anish Nilajkar <anishsnilajkar@gmail.com>
2024-08-17 14:15:53 +0530

    Code for 19th pattern

---
+++ Pattern_19.cpp
@@ -0,0 +1,56 @@
+/*
+
+Pattern 19
+**********
+****  ****
+***    ***
+**      **
+*        *
+*        *
+**      **
+***    ***
+****  ****
+**********
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void pattern19(int n){
+    //!st half
+    for(int i=0;i<n;i++){
+        //star
+        for(int j=0;j<n-i;j++){
+            cout<<"*";
+        };
+        //space
+        for(int j=0;j<i*2;j++){
+            cout<<" ";
+        };
+        //star
+        for(int j=0;j<n-i;j++){
+            cout<<"*";
+        };
+        cout<<endl;
+    };
+    //2nd half
+    for(int i=1;i<=n;i++){
+        //star
+        for(int j=0;j<i;j++){
+            cout<<"*";
+        };
+        //space
+        for(int j=0;j<2*(n-i);j++){
+            cout<<" ";
+        };
+        //star
+        for(int j=0;j<i;j++){
+            cout<<"*";
+        };
+        cout<<endl;
+    };
+}
+int main(){
+    int x;
+    cin>>x;
+    pattern19(x);
+}
